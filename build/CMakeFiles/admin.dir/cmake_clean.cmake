07be5e37eb9c42bd8a346c8b9ff0f8f883c539a0
RiccardoBuzzoni <riccardobuzzoni01@gmail.com>
2025-08-15 16:49:10 +0200

    Pushata la cartella build/.

---
+++ build/CMakeFiles/admin.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/admin.dir/src/admin_client.c.o"
+  "CMakeFiles/admin.dir/src/admin_client.c.o.d"
+  "CMakeFiles/admin.dir/src/utils.c.o"
+  "CMakeFiles/admin.dir/src/utils.c.o.d"
+  "admin"
+  "admin.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/admin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
